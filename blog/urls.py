5944e787b4f209590305d7c7002ca4a2d0a85551
ScorchHu <1093572262@qq.com>
2020-09-05 10:46:09 +0800

    修正目录

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,4 @@
-"""day65报障和博客系统 URL Configuration
+"""blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
